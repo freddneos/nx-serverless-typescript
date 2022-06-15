7cec59d869cb365f73ad5d69c61771da8fd51d14
Frederico Bezerra (Fredd) <36821426+freddneos@users.noreply.github.com>
2022-06-15 12:11:49 +0100

    Initial commit

---
+++ serverless.base.ts
@@ -0,0 +1,33 @@
+import type { Serverless } from 'serverless/aws';
+
+export const baseServerlessConfiguration: Partial<Serverless> = {
+  frameworkVersion: '3',
+  package: {
+    individually: true,
+    excludeDevDependencies: true,
+  },
+  plugins: ['serverless-esbuild', 'serverless-offline'],
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: true,
+      target: 'node14',
+      packager: 'yarn',
+      sourcemap: true,
+      sourcesContent: false,
+    },
+  },
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    memorySize: 128,
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    stage: '${opt:stage}',
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    region: 'eu-west-1',
+  },
+};
